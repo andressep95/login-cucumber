79d4fb83fd91fd0107cdae14fb82c324a5645b5b
Andres Sepulveda <andressep.95@gmail.com>
2025-08-06 21:30:08 -0400

    <feature>: TestRunner config

---
+++ src/test/java/runners/TestRunner.java
@@ -0,0 +1,19 @@
+package runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+    features = "src/test/resources/features",
+    glue = {"steps", "utils"},
+    plugin = {
+        "pretty",
+        "summary",
+        "html:target/cucumber-report.html",
+        "json:target/cucumber-report.json"
+    }
+)
+public class TestRunner {
+}
